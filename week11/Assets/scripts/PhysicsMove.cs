8c6f02bb791bf3b25254943f9fc80012a9d22add
Robert Yang <yang.robert.w@gmail.com>
2015-04-14 01:05:42 -0400

    added week 11: rigidbody input and trigger demo

---
+++ week11/Assets/scripts/PhysicsMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsMove : MonoBehaviour {
+
+	Rigidbody rBody; // cache a reference to the rigidbody we want to access
+
+	// tuning variables
+	public float moveSpeed = 10f;
+	public float turnRate = 100f;
+
+	public float health = 500f; // I lose health in PowerBoost.OnTriggerEnter()
+	
+	void Start () {
+		// which rigidbody we want to talk to
+		rBody = GetComponent<Rigidbody>();
+
+	}
+	
+	// FixedUpdate is called once per physics frame
+	// to see how fast it runs, go to Unity >> Edit Project Settings > Time > Fixed Timestep
+	void FixedUpdate () {
+		// add positional velocity based on W/S or UpArrow/DownArrow
+		rBody.AddForce( transform.forward * Input.GetAxis ( "Vertical" ) * moveSpeed );
+
+		// add angular velocity based on A/D or LeftArrow/RightArrow
+		rBody.AddTorque( new Vector3(0f, turnRate, 0f) * Input.GetAxis ( "Horizontal") );
+
+		// if player presses SPACE then 
+		if (Input.GetKeyDown (KeyCode.Space) ) {
+			rBody.velocity = new Vector3(0f, 0f, 0f); // stops moving
+			rBody.angularVelocity = new Vector3(0f, 0f, 0f); // stops turning
+		}
+	}
+}
+
+
+
---
+++ week11/Assets/scripts/PowerBoost.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerBoost : MonoBehaviour {
+
+	// OnTriggerEnter is a special function that is called automatically upon something
+	// entering a trigger collider on the same object
+	void OnTriggerEnter ( Collider activator ) {
+		Debug.Log ("something entered the trigger!!! omg");
+		activator.GetComponent<Rigidbody>().velocity *= 5f; // increase speed by 500%
+		Destroy ( this.gameObject ); // self-destruct so this powerup is used-up
+
+		// access a public variable on my own script
+		activator.GetComponent<PhysicsMove>().health -= 10f;
+	}
+}
