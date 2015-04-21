ec9857d085733ae25bd14fb27652ebaf7b9d26b0
Robert Yang <yang.robert.w@gmail.com>
2015-04-21 14:22:46 -0400

    week 13: screen shake, smooth follow, trail renderer

---
+++ week13/Assets/scripts/ScreenShake.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour {
+
+	// public static vars = accessible by ANYWHERE IN CODEBASE, and they PERSIST THROUGH LEVEL CHANGES
+	public static float shakeStrength = 0f;
+
+	Vector3 whereWeStarted;
+	Vector3 randomOffset;
+
+	// Use this for initialization
+	void Start () {
+		whereWeStarted = Camera.main.transform.position;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		randomOffset = Random.insideUnitSphere;
+
+		Camera.main.transform.position = Vector3.Lerp ( 
+		                                               Camera.main.transform.position, 
+		                                               whereWeStarted + randomOffset * shakeStrength, 
+		                                               Time.deltaTime * 10f 
+		                                              );
+
+		// gradually reduce to zero
+		shakeStrength = Mathf.Lerp ( shakeStrength, 0f, Time.deltaTime * 10f );
+
+		// DEBUG
+		if ( Application.isEditor && Input.GetKeyDown(KeyCode.Space) ) {
+			shakeStrength = 5f;
+		}
+	}
+}
---
+++ week13/Assets/scripts/SmoothFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour {
+
+	public Transform followThis; // assign in Inspector
+
+	bool didIShakeAlready;
+
+	// Update is called once per frame
+	void Update () {
+		// instantly override current position
+	//	transform.position = followThis.position;
+
+		transform.position = Vector3.Lerp ( transform.position, followThis.position, Time.deltaTime * 10f );
+
+		if ( Vector3.Distance ( transform.position, followThis.position ) < 1f ) {
+			if (didIShakeAlready == false) {
+				ScreenShake.shakeStrength = 5f;
+				didIShakeAlready = true;
+			}
+		} else {
+			didIShakeAlready = false;
+		}
+
+	}
+}
