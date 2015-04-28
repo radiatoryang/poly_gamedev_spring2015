2642cfc8e9d83f83dcc2a53388d868b3b23acca9
Robert Yang <yang.robert.w@gmail.com>
2015-04-28 14:47:11 -0400

    week 14: playing sounds, static vars and playerprefs

---
+++ week14/Assets/scripts/Scorekeeper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scorekeeper : MonoBehaviour {
+
+	// easiest way to keep score between scene changes
+	// is to use a STATIC variable
+
+	public static int player1Score, player2Score;
+
+	// Use this for initialization
+	void Start () {
+		if ( PlayerPrefs.HasKey ( "p1Score" ) ) {
+			player1Score = PlayerPrefs.GetInt ( "p1Score" );
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown (KeyCode.Space) ) {
+			player1Score++;
+			player2Score++;
+			Debug.Log ( player1Score );
+		}
+
+		if (Input.GetKeyDown (KeyCode.P) ) {
+			Debug.Log ("saved to player prefs");
+			PlayerPrefs.SetInt ( "p1Score", player1Score );
+			PlayerPrefs.SetInt ( "p2Score", player2Score );
+			PlayerPrefs.Save ();
+		}
+
+
+	}
+}
---
+++ week14/Assets/scripts/SoundDemo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDemo : MonoBehaviour {
+
+	bool shouldIPlayASound;
+
+	public AudioClip screamSound1;
+
+	// Update is called once per frame
+	void Update () {
+		// "one shot"
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			AudioSource.PlayClipAtPoint( screamSound1, transform.position, 1f );
+			Invoke ( "StartGame", 3f );
+		}
+
+		// loop a sound after a certain event
+		if (shouldIPlayASound && GetComponent<AudioSource>().isPlaying == false) {
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	void StartGame () {
+		Application.LoadLevel ( "mainGameScene" );
+	}
+}
